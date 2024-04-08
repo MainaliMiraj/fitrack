67d85dd61df5655eca6e9ca7e0be9ba8326d1f1e
Miraj Mainali <merazzmainali05@gmail.com>
2024-04-08 19:56:05 +1000

    added the bmi message

--- src/components/BMICalculator/BmiCalculator.tsx
+++ src/components/BMICalculator/BmiCalculator.tsx
@@ -4,39 +4,56 @@ import React, { useState } from "react";
 const BmiCalculator = () => {
   const [height, setHeight] = useState<number | "">("");
   const [weight, setWeight] = useState<number | "">("");
+  const [bmiResult, setBmiResult] = useState<number | null>(null);
 
   function calculateBMI(): void {
     if (typeof height === "number" && typeof weight === "number") {
       const bmi = weight / ((height / 100) * (height / 100));
-      console.log("BMI:", bmi);
+      setBmiResult(bmi);
     } else {
       console.error("Invalid height or weight");
     }
   }
 
+  function getBmiMessage(bmi: number): string {
+    if (bmi < 18.5) {
+      return `You are underweight. Consider strength training exercises at least 3 times a week and have enough protein (Your weight in kg * 1.2) gm.`;
+    } else if (bmi < 25) {
+      return "Your weight is normal. Keep up your health and eat healthy food and exercise more.";
+    } else if (bmi < 30) {
+      return "You are overweight. Please check your daily calorie intake and try to consume 300-400 fewer calories than your maintenance calorie.";
+    } else {
+      return "You are obese. Please consult a dietitian about your weight loss journey for your healthy life.";
+    }
+  }
+
   return (
     <div className="bg-gradient-to-b from-customGreen to-green-700 h-2/3 w-2/4 m-4 p-4 rounded-lg">
-      <div className="text-white font-bold text-3xl">Calculate your BMI</div>
+      <div className="text-white font-cbold text-3xl">Calculate your BMI</div>
       <div className="flex bg-transparent  h-2/3 flex-col items-center justify-center shadow-lg gap-y-6 w-full p-5  ">
         <div>
           <span>Height:</span>
           <input
-            type="text"
+            type="number"
             className="p-1.5"
             placeholder="in Cm"
-            value={height}
-            onChange={(e) => setHeight(Number(e.target.value))}
+            value={height === "" ? "" : height}
+            onChange={(e) =>
+              setHeight(e.target.value === "" ? "" : Number(e.target.value))
+            }
           ></input>
         </div>
 
         <div>
           <span>Weight:</span>
           <input
-            type="text"
+            type="number"
             className="p-1.5"
             placeholder="in Kg"
-            value={weight}
-            onChange={(e) => setWeight(Number(e.target.value))}
+            value={weight === "" ? "" : weight}
+            onChange={(e) =>
+              setWeight(e.target.value === "" ? "" : Number(e.target.value))
+            }
           ></input>
         </div>
 
@@ -47,6 +64,17 @@ const BmiCalculator = () => {
           Calculate your BMI
         </button>
       </div>
+      {bmiResult !== null && (
+        <div
+          className={`text-white ${
+            bmiResult !== null && (bmiResult < 18.5 || bmiResult >= 25)
+              ? "text-red-500"
+              : "text-green-500"
+          }`}
+        >
+          {getBmiMessage(bmiResult)}
+        </div>
+      )}
     </div>
   );
 };

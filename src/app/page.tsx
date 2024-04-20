45338f36b05d119a7ed2cc61318ffeeb63bd06e1
Miraj Mainali <merazzmainali05@gmail.com>
2024-04-20 20:18:55 +1000

    added the searc component to search the exercises

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import BmiCalculator from "@/components/BMICalculator/BmiCalculator";
-
+import SearchExercise from "@/components/SearchExercise/SearchExercise";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center">
-      <BmiCalculator/>
-      
+    <div className="flex items-center justify-center flex-col gap-2 p-4 ">
+      <SearchExercise />
+      <BmiCalculator />
     </div>
   );
 }
--- src/components/SearchExercise/SearchExercise.tsx
+++ src/components/SearchExercise/SearchExercise.tsx
@@ -1,7 +1,22 @@
+"use client";
 import React from "react";
+import { useState } from "react";
 
 const SearchExercise = () => {
-  return <div>SearchExercise</div>;
+  const [searchData, setSearchData] = useState("");
+  console.log(searchData)
+  return (
+    <div className="flex gap-4 w-full items-center justify-center">
+      <input
+        type="text"
+        placeholder="What are we doing today?"
+        className="w-2/3 border border-black p-3"
+        onChange={(e) => {
+          setSearchData(e.target.value);
+        }}
+      />
+    </div>
+  );
 };
 
 export default SearchExercise;
